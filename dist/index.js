d0a6136b0e4fd384143a15822d1747435c85ab6f
Clark Malmgren <cmalmgren@gmail.com>
2016-02-14 17:16:08 -0600

    Add an index.js file so that both files can be exported without a circular dependency.

---
+++ dist/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.RunnablePromise = exports.ExtendablePromise = undefined;
+
+var _extendablePromise = require('./extendable-promise');
+
+var _extendablePromise2 = _interopRequireDefault(_extendablePromise);
+
+var _runnablePromise = require('./runnable-promise');
+
+var _runnablePromise2 = _interopRequireDefault(_runnablePromise);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.ExtendablePromise = _extendablePromise2.default;
+exports.RunnablePromise = _runnablePromise2.default;
+exports.default = _extendablePromise2.default;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImluZGV4LmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7O1FBR1M7UUFBbUIiLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgRXh0ZW5kYWJsZVByb21pc2UgZnJvbSAnLi9leHRlbmRhYmxlLXByb21pc2UnO1xuaW1wb3J0IFJ1bm5hYmxlUHJvbWlzZSBmcm9tICcuL3J1bm5hYmxlLXByb21pc2UnO1xuXG5leHBvcnQgeyBFeHRlbmRhYmxlUHJvbWlzZSwgUnVubmFibGVQcm9taXNlIH07XG5leHBvcnQgZGVmYXVsdCBFeHRlbmRhYmxlUHJvbWlzZTsiXSwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ExtendablePromise from './extendable-promise';
+import RunnablePromise from './runnable-promise';
+
+export { ExtendablePromise, RunnablePromise };
+export default ExtendablePromise;
